39c5601784db4c2a80a75d6813b89ebd1c01d29e
Roger Creyke <roger.creyke@odds-bods.com>
2018-09-11 20:18:43 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WordFinder
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await File.WriteAllTextAsync("words.txt",
+                await new HttpClient().GetStringAsync("https://github.com/dwyl/english-words/raw/master/words.txt"));
+
+            var validWords = (await File.ReadAllLinesAsync("words.txt"))
+                .Select(x =>
+                {
+                    var w = x.ToLowerInvariant();
+                    return (
+                        w,
+                        p: w.Sum(y => y - 96),
+                        m: $"{(string.Join("+", w.ToCharArray().Select(p => (p - 96).ToString()).ToArray()))} = 100"
+                    );
+                })
+                .Where(x => !x.w.Any(y => y < 97 || y > 122));
+
+            var matchedWords = validWords.Where(x => x.p == 100);
+
+            matchedWords.ToList().ForEach(x => Console.WriteLine($"{x.w} {x.m}"));
+
+            Console.WriteLine($"Total: {matchedWords.Count()}/{validWords.Count()}");
+            Console.Read();
+        }
+    }
+}
